9152e334bede03e7128a2ecfe84db2b3c72e03a2
shekhovtsovsergey <98221024+shekhovtsovsergey@users.noreply.github.com>
2022-12-03 16:49:40 +0300

    Initial commit

---
+++ desktop/src/ru/geekbrains/stargame/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package ru.geekbrains.stargame;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import ru.geekbrains.stargame.StarGame;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("StarGame");
+		new Lwjgl3Application(new StarGame(), config);
+	}
+}
---
+++ ios/src/ru/geekbrains/stargame/IOSLauncher.java
@@ -0,0 +1,22 @@
+package ru.geekbrains.stargame;
+
+import org.robovm.apple.foundation.NSAutoreleasePool;
+import org.robovm.apple.uikit.UIApplication;
+
+import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
+import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
+import ru.geekbrains.stargame.StarGame;
+
+public class IOSLauncher extends IOSApplication.Delegate {
+    @Override
+    protected IOSApplication createApplication() {
+        IOSApplicationConfiguration config = new IOSApplicationConfiguration();
+        return new IOSApplication(new StarGame(), config);
+    }
+
+    public static void main(String[] argv) {
+        NSAutoreleasePool pool = new NSAutoreleasePool();
+        UIApplication.main(argv, null, IOSLauncher.class);
+        pool.close();
+    }
+}
